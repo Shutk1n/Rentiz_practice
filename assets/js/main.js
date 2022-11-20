16b237626c9bb22aa7357b006cce88c135bf1c7c
Shutk1n <puppysky.syper8@gmail.com>
2022-11-20 19:45:54 +0200

    first page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+// --- Burger Menu
+
 const burger = document.querySelector('.menu-icon');
 const menu = document.querySelector('.menu');
 const body = document.body
@@ -7,5 +9,24 @@ if (burger && menu) {
     burger.classList.toggle('_active');
     menu.classList.toggle('_active');
     body.classList.toggle('_lock');
+  })
+}
+
+// ---- Filter Dropdonw
+
+const filter = document.querySelector('.filter');
+
+if (filter) {
+  const items = filter.querySelectorAll('.block-filter')
+
+  items.forEach(item => {
+    item.addEventListener('click', event => {
+      item.querySelector('.block-filter__dropdown').classList.toggle('_active');
+      item.querySelector('.block-filter__icon').classList.toggle('_active');
+
+      if (event.target.classList.contains('block-filter__item')) {
+        item.querySelector('.block-filter__value').textContent = event.target.textContent;
+      }
+    })
   })
 }
