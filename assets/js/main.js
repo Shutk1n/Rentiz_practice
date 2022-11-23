68ee3f792bdc8962fb2f0adfb8d7484ab637a804
Shutk1n <puppysky.syper8@gmail.com>
2022-11-23 18:37:27 +0200

    all done!

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,3 +60,23 @@ const reviewsSlider = new Swiper('.slider-reviews', {
     prevEl: '.slider-reviews-prev',
   },
 });
+
+
+
+//----------Gallery
+
+
+const galleryItems = document.querySelectorAll('.gallery__item');
+
+if (galleryItems.length > 0) {
+  galleryItems.forEach(item => {
+    new Swiper(item, {
+      slidesPerView: 1,
+      autoplay: {
+        delay: 4000,
+      },
+      effect: 'fade',
+    })
+  })
+}
+
